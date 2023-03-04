cf2959753722dfa3e4f5a28a4fedf818016b4816
crowwan <crowwan@naver.com>
2023-03-04 18:14:27 +0900

    style: 파일 확장자 변경 js => jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import GlobalStyles from "./components/GlobalStyles";
+import ImportantPage from "./pages/ImportantPage";
+import PlannedPage from "./pages/PlannedPage";
+import TodayPage from "./pages/TodayPage";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/today" element={<TodayPage />} />
+        <Route path="/important" element={<ImportantPage />} />
+        <Route path="/planned" element={<PlannedPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
