006bfa2a9e31965faf720c14a6747ff9010a675c
crowwan <crowwan@naver.com>
2023-03-05 22:28:19 +0900

    feat: 파일 위치 이동

--- src/components/StyledContainer.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const StyledContainer = styled.div`
-  background-color: ${(props) => props.bgColor || "#1E1E1E"};
-  padding: 0 1rem;
-`;
-
-export default StyledContainer;
