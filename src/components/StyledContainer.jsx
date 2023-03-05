bb3e44064661df460cad86af6c77ab18acba6263
crowwan <crowwan@naver.com>
2023-03-05 19:58:15 +0900

    feat: 공통 컨테이너 컴포넌트 생성

---
+++ src/components/StyledContainer.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  background-color: ${(props) => props.bgColor};
+  padding: 0 1rem;
+`;
+
+export default StyledContainer;
