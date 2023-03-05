0fad3f0032422b6fc7e3965a35694f092efcbecd
crowwan <crowwan@naver.com>
2023-03-05 19:57:40 +0900

    feat: 해더 스타일드 컴포넌트 적용 중

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import StyledContainer from "../components/StyledContainer";
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  height: 3rem;
+  color: #fff;
+`;
+const StyledNavButton = styled.span``;
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <StyledContainer bgColor={"#252423"}>
+      <StyledHeader>Header</StyledHeader>
+    </StyledContainer>
+  );
 }
 
 export default Header;
