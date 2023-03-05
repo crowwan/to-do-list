19e2cb4fb5124ee91f6df7f716fac16655d544a9
crowwan <crowwan@naver.com>
2023-03-05 20:49:15 +0900

    feat: Create Menu btn css

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,14 +6,40 @@ const StyledHeader = styled.header`
   display: flex;
   align-items: center;
   height: 3rem;
+  gap: 1.2rem;
   color: #fff;
 `;
-const StyledNavButton = styled.span``;
+const StyledNavButton = styled.div`
+  cursor: pointer;
+  position: relative;
+  width: 25px;
+  height: 3px;
+  background-color: #fff;
+  &::after {
+    content: "";
+    position: absolute;
+    top: -8px;
+    width: 25px;
+    height: 3px;
+    background-color: #fff;
+  }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 8px;
+    width: 25px;
+    height: 3px;
+    background-color: #fff;
+  }
+`;
 
 function Header() {
   return (
     <StyledContainer bgColor={"#252423"}>
-      <StyledHeader>Header</StyledHeader>
+      <StyledHeader>
+        <StyledNavButton />
+        <div>To Do</div>
+      </StyledHeader>
     </StyledContainer>
   );
 }
