215d7f09f86f3ce57d2828fdb6eed8940e07e51d
crowwan <crowwan@naver.com>
2023-03-07 00:07:45 +0900

    feat: nav 컴포넌트 구현

--- src/layouts/Nav.jsx
+++ src/layouts/Nav.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import routerData from "../data/routerData";
+import StyledContainer from "../styled/StyledContainer";
+import StyledNavItem from "../styled/StyledNavItem";
+
+const StyledNavContainer = styled(StyledContainer)`
+  width: 80vw;
+  position: absolute;
+  top: 3rem;
+  height: calc(100vh - 3rem);
+  padding-top: 2rem;
+  color: #fff;
+`;
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <StyledNavContainer>
+      <div>To Do</div>
+      {routerData.map((a) => (
+        <StyledNavItem icon={a.icon} title={a.name} />
+      ))}
+    </StyledNavContainer>
+  );
 }
 
 export default Nav;
