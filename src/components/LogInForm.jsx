75c57f89b06ac0c934097d1edbe74acfcb5d7817
crowwan <crowwan@naver.com>
2023-03-06 15:49:17 +0900

    feat: 로그인 폼 컴포넌트 생성 및 ui 완료

--- src/components/LogInForm.jsx
+++ src/components/LogInForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import StyledMainTitle from "../styled/StyledMainTitle";
-
+import StyledInput from "../styled/StyledInput";
 const StyledContainer = styled.div`
   background-color: #252423;
   border-radius: 10px;
@@ -9,11 +9,39 @@ const StyledContainer = styled.div`
   padding: 1rem;
 `;
 
-const StyledInput = styled.div``;
+const StyledSubTitle = styled(StyledMainTitle)`
+  font-size: 14px;
+  font-weight: 400;
+`;
+const StyledLogInInput = styled(StyledInput)`
+  background-color: #1e1e1e;
+  border: none;
+  margin-bottom: 1rem;
+`;
+const StyledSubmitBtn = styled.button`
+  background-color: #82abf1;
+  color: #fff;
+  border: none;
+  padding: 0.2rem 0.5rem;
+  border-radius: 5px;
+`;
+const StyledBtnContainer = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 2rem;
+`;
 function LogInForm() {
   return (
     <StyledContainer>
       <StyledMainTitle>로그인 / 회원가입</StyledMainTitle>
+      <StyledSubTitle>아이디</StyledSubTitle>
+      <StyledLogInInput type="text" placeholder={"아이디"} />
+      <StyledSubTitle>비밀번호</StyledSubTitle>
+      <StyledLogInInput type="password" placeholder={"비밀번호"} />
+      <StyledBtnContainer>
+        <StyledSubmitBtn>회원가입</StyledSubmitBtn>
+        <StyledSubmitBtn>로그인</StyledSubmitBtn>
+      </StyledBtnContainer>
     </StyledContainer>
   );
 }
