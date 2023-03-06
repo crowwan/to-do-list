7af6c32d15b0023266c5a37c09f65c9ea2cb8cd2
crowwan <crowwan@naver.com>
2023-03-07 00:04:14 +0900

    feat: nav 컴포넌트 추가, 클릭 핸들러 구현

--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../layouts/Header";
-
+import Nav from "../layouts/Nav";
 function Template() {
+  const [open, setOpen] = useState(false);
+  const onNavBtnClick = () => {
+    setOpen((prev) => !prev);
+  };
   return (
     <div>
-      <Header />
+      <Header onNavBtnClick={onNavBtnClick} />
+      {open && <Nav />}
       <Outlet />
     </div>
   );
