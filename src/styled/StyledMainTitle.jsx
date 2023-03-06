d2bcbd1cb49950174ff6c90b6fa10542bba190d0
crowwan <crowwan@naver.com>
2023-03-06 14:04:39 +0900

    feat: StyledMainTitle 컴포넌트 추출

---
+++ src/styled/StyledMainTitle.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+const StyledMainTitle = styled.h3`
+  color: #fff;
+  font-size: 1.1rem;
+  font-weight: 500;
+  margin-bottom: 2rem;
+`;
+export default StyledMainTitle;
