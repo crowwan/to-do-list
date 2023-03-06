5ed6cbd83a80b4b94425fcb7d5bd97aa90099ab2
crowwan <crowwan@naver.com>
2023-03-06 14:07:28 +0900

    feat: root route 연결 컴포넌트 변경
    Template -> LogInPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import GlobalStyles from "./components/GlobalStyles";
 import ImportantPage from "./pages/ImportantPage";
+import LogInPage from "./pages/LogInPage";
 import PlannedPage from "./pages/PlannedPage";
 import Template from "./pages/Template";
 import TodayPage from "./pages/TodayPage";
 
 function App() {
+  const [test, setTest] = useState("");
+  console.log("render");
+  useEffect(() => {
+    // (async () => {
+    //   const data = await fetch("http://localhost:8000").then((res) =>
+    //     res.json()
+    //   );
+    //   setTest(data);
+    // })();
+    console.log(document.cookie);
+  }, []);
   return (
     <>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Template />}>
+        <Route path="/" element={<LogInPage />} />
+        <Route path="/user" element={<Template />}>
           <Route path="today" element={<TodayPage />} />
           <Route path="important" element={<ImportantPage />} />
           <Route path="planned" element={<PlannedPage />} />
