07accc75f7f58a8f5562c7fc51828232881c18f9
crowwan <crowwan@naver.com>
2023-03-07 22:19:08 +0900

    feat: 인풋창 버튼 ui 수정

--- src/components/UtilButtonContainer.jsx
+++ src/components/UtilButtonContainer.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { FaCalendarAlt } from "react-icons/fa";
+
 const StyledContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-wrap: wrap;
   align-items: center;
   background-color: #252423;
-  padding: 0.8rem 1.5rem;
   font-size: 20px;
   width: 100%;
   & * {
@@ -14,18 +13,65 @@ const StyledContainer = styled.div`
   }
 `;
 
+const StyledTypeBtnContainer = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 2rem;
+  padding-left: 0.5rem;
+  margin-bottom: 2rem;
+`;
+const StyledTypeBtn = styled.button`
+  cursor: pointer;
+  border: none;
+  font-weight: bold;
+  color: ${(props) => (props.checked ? "#4e87e9" : "#ffffff")};
+  background-color: transparent;
+  font-size: 16px;
+`;
+
 const StyledSubmitBtn = styled.button`
   background-color: transparent;
   padding: 0.2rem 1rem;
   font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  width: 100%;
+  height: 2rem;
   color: #fff;
-  border: 1px solid #fff;
+  background-color: #4e87e9;
 `;
 
 function UtilButtonContainer() {
+  const [type, setType] = useState([
+    {
+      name: "Today",
+      checked: true,
+    },
+    {
+      name: "Everyday",
+      checked: false,
+    },
+  ]);
+  const onTypeBtnClick = (name) => {
+    setType((prev) =>
+      prev.map((a) =>
+        a.name === name ? { ...a, checked: true } : { ...a, checked: false }
+      )
+    );
+  };
   return (
     <StyledContainer>
-      <FaCalendarAlt />
+      <StyledTypeBtnContainer>
+        {type.map((a) => (
+          <StyledTypeBtn
+            onClick={() => onTypeBtnClick(a.name)}
+            checked={a.checked}
+            key={a.name}
+          >
+            {a.name}
+          </StyledTypeBtn>
+        ))}
+      </StyledTypeBtnContainer>
       <StyledSubmitBtn>추가</StyledSubmitBtn>
     </StyledContainer>
   );
