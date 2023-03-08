071db5fa4f94c2ec4378d2b434998fde1f93d9d6
crowwan <crowwan@naver.com>
2023-03-09 01:17:37 +0900

    feat: todoitem modal 생성

--- src/ui/ToDoItemModal.jsx
+++ src/ui/ToDoItemModal.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #1e1e1e;
+  color: #fff;
+`;
+const StyledModalContainer = styled.div``;
 
 function ToDoItemModal() {
-  return <div>ToDoItemModal</div>;
+  return <StyledWrapper>ToDoItemModal</StyledWrapper>;
 }
 
 export default ToDoItemModal;
