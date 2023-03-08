eaeaa351f18124aa3d793275579fe45799ee88f6
crowwan <crowwan@naver.com>
2023-03-09 01:20:59 +0900

    feat: 로그아웃 버튼 구현

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { setIsLogin } from "../features/loginSlice";
 import StyledContainer from "../styled/StyledContainer";
 
 const StyledHeader = styled.header`
@@ -8,6 +10,9 @@ const StyledHeader = styled.header`
   height: 3rem;
   gap: 1.2rem;
   color: #fff;
+  & > div:nth-child(2) {
+    flex: 1 0 0;
+  }
 `;
 const StyledNavButton = styled.div`
   cursor: pointer;
@@ -32,13 +37,22 @@ const StyledNavButton = styled.div`
     background-color: #fff;
   }
 `;
-
+const StyledLogoutBtn = styled.button`
+  background-color: transparent;
+  color: #fff;
+  border: none;
+`;
 function Header({ onNavBtnClick }) {
+  const dispatch = useDispatch();
+  const onLogoutClick = () => {
+    dispatch(setIsLogin(false));
+  };
   return (
     <StyledContainer bgColor={"#252423"}>
       <StyledHeader>
         <StyledNavButton onClick={onNavBtnClick} />
         <div>To Do</div>
+        <StyledLogoutBtn onClick={onLogoutClick}>Log Out</StyledLogoutBtn>
       </StyledHeader>
     </StyledContainer>
   );
