091a23a8c031090caf6f29248c2750e2e981e130
crowwan <crowwan@naver.com>
2023-03-08 22:03:00 +0900

    feat: isLogin 리듀서 추가

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
+import isLoginReducer from "../features/loginSlice";
 const store = configureStore({
-  reducers: {
+  reducer: {
     user: userReducer,
+    isLogin: isLoginReducer,
   },
 });
 export default store;
