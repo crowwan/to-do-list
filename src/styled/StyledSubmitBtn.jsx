24125d9f3625dc88a3b5074c411fb3d420fe3a2a
crowwan <crowwan@naver.com>
2023-03-09 01:18:37 +0900

    feat: StyledSubmitBtn 컴포넌트 추출

---
+++ src/styled/StyledSubmitBtn.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+const StyledSubmitBtn = styled.button`
+  background-color: ${(props) => props.bgc || "#82abf1"};
+  width: 100%;
+  color: #fff;
+  border: none;
+  padding: 0.5rem;
+  border-radius: 5px;
+  font-weight: bold;
+  margin-top: 1rem;
+`;
+export default StyledSubmitBtn;
