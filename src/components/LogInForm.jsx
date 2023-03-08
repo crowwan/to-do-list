9aacb0097ef09282e6a2252145fd588367f9fd62
crowwan <crowwan@naver.com>
2023-03-09 01:19:42 +0900

    feat: 회원가입 기능 확인

--- src/components/LogInForm.jsx
+++ src/components/LogInForm.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import StyledMainTitle from "../styled/StyledMainTitle";
 import StyledInput from "../styled/StyledInput";
+import StyledSubmitBtn from "../styled/StyledSubmitBtn";
+import { useDispatch } from "react-redux";
+import { setIsLogin } from "../features/loginSlice";
+import { useNavigate } from "react-router-dom";
 const StyledContainer = styled.div`
   background-color: #252423;
   border-radius: 10px;
@@ -12,38 +16,60 @@ const StyledContainer = styled.div`
 const StyledSubTitle = styled(StyledMainTitle)`
   font-size: 14px;
   font-weight: 400;
+  margin-bottom: 1rem;
 `;
 const StyledLogInInput = styled(StyledInput)`
   background-color: #1e1e1e;
   border: none;
   margin-bottom: 1rem;
 `;
-const StyledSubmitBtn = styled.button`
-  background-color: #82abf1;
-  color: #fff;
-  border: none;
-  padding: 0.2rem 0.5rem;
-  border-radius: 5px;
-  font-weight: bold;
-`;
-const StyledBtnContainer = styled.div`
+
+const StyledSignupContainer = styled.div`
   display: flex;
   width: 100%;
   gap: 2rem;
+  font-size: 12px;
+  flex-direction: row-reverse;
   margin-top: 1rem;
+  & span {
+    color: #82abf1;
+    text-decoration: none;
+  }
 `;
 function LogInForm() {
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+  const navigation = useNavigate();
+  const idRef = useRef();
+  const pwRef = useRef();
+
+  const onSignupClick = () => {
+    setIsSignup(true);
+  };
+
+  const onLoginClick = () => {
+    console.log(idRef.current.value, pwRef.current.value);
+    dispatch(setIsLogin(true));
+    navigation("/today");
+  };
+
   return (
     <StyledContainer>
-      <StyledMainTitle>로그인 / 회원가입</StyledMainTitle>
+      <StyledMainTitle>{isSignup ? "회원가입" : "로그인"}</StyledMainTitle>
       <StyledSubTitle>아이디</StyledSubTitle>
-      <StyledLogInInput type="text" placeholder={"아이디"} />
+      <StyledLogInInput type="text" placeholder={"아이디"} ref={idRef} />
       <StyledSubTitle>비밀번호</StyledSubTitle>
-      <StyledLogInInput type="password" placeholder={"비밀번호"} />
-      <StyledBtnContainer>
-        <StyledSubmitBtn>회원가입</StyledSubmitBtn>
-        <StyledSubmitBtn>로그인</StyledSubmitBtn>
-      </StyledBtnContainer>
+      <StyledLogInInput type="password" placeholder={"비밀번호"} ref={pwRef} />
+      {!isSignup ? (
+        <>
+          <StyledSubmitBtn onClick={onLoginClick}>로그인</StyledSubmitBtn>
+          <StyledSignupContainer>
+            <span onClick={onSignupClick}>회원가입</span>
+          </StyledSignupContainer>
+        </>
+      ) : (
+        <StyledSubmitBtn onClick={onLoginClick}>회원가입</StyledSubmitBtn>
+      )}
     </StyledContainer>
   );
 }
