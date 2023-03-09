8bd945c668e7258d21bcdd3fde13c7eb6dd3de79
crowwan <crowwan@naver.com>
2023-03-10 00:22:15 +0900

    feat: data상태 추가

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
 import isLoginReducer from "../features/loginSlice";
+import dataReducer from "../features/dataSlice";
 const store = configureStore({
   reducer: {
     user: userReducer,
     isLogin: isLoginReducer,
+    data: dataReducer,
   },
 });
 export default store;
