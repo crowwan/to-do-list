57f6ce855717e9deec38d8c1dfcb3c09510f934a
crowwan <crowwan@naver.com>
2023-03-11 21:23:46 +0900

    feat: 에러 메시지 컴포넌트 추출

---
+++ src/styled/StyledErrorMsg.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+const StyledErrorMsg = styled.div`
+  color: #ff6d6d;
+  width: 100%;
+  text-align: center;
+  margin-top: 1rem;
+`;
+
+export default StyledErrorMsg;
