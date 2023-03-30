dc1fd7d925b4948a26a6250db9abcbbbcc441209
crowwan <crowwan@naver.com>
2023-03-30 17:57:30 +0900

    feat: 미사용 컴포넌트 삭제로 인한 코드 수정 - 페이지 별 컴포넌트 삭제

--- src/data/routerData.js
+++ src/data/routerData.js
@@ -1,25 +1,20 @@
-import TodayPage from "../pages/TodayPage";
 import { BsSun } from "react-icons/bs";
 import { AiOutlineCalendar, AiFillStar } from "react-icons/ai";
-import EverydayPage from "../pages/EverydayPage";
-import ImportantPage from "../pages/ImportantPage";
+
 const routerData = [
   {
     name: "오늘 할 일",
     path: "today",
-    element: <TodayPage />,
     icon: <BsSun />,
   },
   {
     name: "매일 할 일",
     path: "everyday",
-    element: <EverydayPage />,
     icon: <AiOutlineCalendar />,
   },
   {
     name: "중요한 일",
     path: "important",
-    element: <ImportantPage />,
     icon: <AiFillStar />,
   },
 ];
