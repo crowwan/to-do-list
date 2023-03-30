59f218ddc9b7f7f78c7c590b8ed33716016296b7
crowwan <crowwan@naver.com>
2023-03-30 17:56:12 +0900

    feat: 미사용 컴포넌트 삭제

--- src/pages/EverydayPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Main from "../layouts/Main";
-function EverydayPage() {
-  return <Main title="매일할 일" />;
-}
-
-export default EverydayPage;
--- src/pages/TodayPage.jsx
+++
@@ -1,7 +0,0 @@
-import Main from "../layouts/Main";
-
-function TodayPage() {
-  return <Main title="오늘 할 일" />;
-}
-
-export default TodayPage;
