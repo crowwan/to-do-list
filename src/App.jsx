c02d875a3d7966012da2846671dac89dc4624586
crowwan <crowwan@naver.com>
2023-03-30 16:21:09 +0900

    feat: 로딩 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import GlobalStyles from "./components/GlobalStyles";
@@ -7,11 +7,12 @@ import { setData } from "./features/dataSlice";
 import Main from "./layouts/Main";
 import LogInPage from "./pages/LogInPage";
 import Template from "./pages/Template";
+import Loading from "./ui/Loading";
 import { getTodos } from "./util/todos";
 
 function App() {
   // TODO: login slice delete
-
+  const [isLoading, setIsLoading] = useState(false);
   const currentUser = useSelector((s) => s.user);
   const dispatch = useDispatch();
   const navigation = useNavigate();
@@ -20,9 +21,11 @@ function App() {
     if (!currentUser) {
       navigation("/");
     } else {
+      setIsLoading(true);
       getTodos()
         .then((res) => {
           dispatch(setData(res.data));
+          setIsLoading(false);
         })
         .catch((err) => {
           console.log(err);
@@ -32,7 +35,7 @@ function App() {
   return (
     <>
       <GlobalStyles />
-
+      {isLoading && <Loading />}
       <Routes>
         <Route path="/" element={!currentUser ? <LogInPage /> : <Template />}>
           {routerData.map((a) => (
