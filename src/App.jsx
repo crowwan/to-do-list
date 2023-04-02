de04ac3524a4ed8915bbbefe6555e2483e747ea6
crowwan <crowwan@naver.com>
2023-04-02 21:18:07 +0900

    feat: 스타일드 컴포넌트 테마 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes, useNavigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./components/GlobalStyles";
 import routerData from "./data/routerData";
 import { setData } from "./features/dataSlice";
@@ -10,9 +11,35 @@ import Template from "./pages/Template";
 import Loading from "./ui/Loading";
 import { getTodos } from "./util/todos";
 
+const themes = {
+  light: {
+    mainColor: "#2564cf",
+    bgColor: "#e1dfdd",
+    navBgColor: "#82abf1",
+    headerColor: "#2564cf",
+    inputColor: "#f3f2f1",
+    bgSubColor: "#fff",
+    todoColor: "#f3f2f1",
+    fontColor: "#323130",
+    hover: "#fdfdfd",
+  },
+  dark: {
+    mainColor: "#82abf1",
+    bgColor: "#1E1E1E",
+    navBgColor: "#1E1E1E",
+    headerColor: "#252423",
+    inputColor: "#252423",
+    todoColor: "#252423",
+    bgSubColor: "#5f5f5f",
+    fontColor: "#fff",
+    hover: "#3b3a39",
+  },
+};
+
 function App() {
   // TODO: login slice delete
   const [isLoading, setIsLoading] = useState(false);
+  const [theme, setTheme] = useState("dark");
   const currentUser = useSelector((s) => s.user);
   const dispatch = useDispatch();
   const navigation = useNavigate();
@@ -34,22 +61,29 @@ function App() {
   }, [currentUser]);
   return (
     <>
-      <GlobalStyles />
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Routes>
-          <Route path="/" element={!currentUser ? <LogInPage /> : <Template />}>
-            {routerData.map((a) => (
-              <Route
-                key={a.name}
-                path={a.path}
-                element={<Main title={a.name} path={a.path} />}
-              />
-            ))}
-          </Route>
-        </Routes>
-      )}
+      <ThemeProvider theme={themes[theme]}>
+        <GlobalStyles />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                !currentUser ? <LogInPage /> : <Template setTheme={setTheme} />
+              }
+            >
+              {routerData.map((a) => (
+                <Route
+                  key={a.name}
+                  path={a.path}
+                  element={<Main title={a.name} path={a.path} />}
+                />
+              ))}
+            </Route>
+          </Routes>
+        )}
+      </ThemeProvider>
     </>
   );
 }
