589a0888b1469b30517430870d8d55d1004df792
crowwan <crowwan@naver.com>
2023-04-04 11:41:24 +0900

    feat: Add themeData

---
+++ src/data/themeData.js
@@ -0,0 +1,24 @@
+export const themes = {
+  light: {
+    mainColor: "#2564cf",
+    bgColor: "#e1dfdd",
+    navBgColor: "#82abf1",
+    headerColor: "#2564cf",
+    inputColor: "#f3f2f1",
+    bgSubColor: "#fff",
+    todoColor: "#f3f2f1",
+    fontColor: "#323130",
+    hover: "#fdfdfd",
+  },
+  dark: {
+    mainColor: "#82abf1",
+    bgColor: "#1E1E1E",
+    navBgColor: "#1E1E1E",
+    headerColor: "#252423",
+    inputColor: "#252423",
+    todoColor: "#252423",
+    bgSubColor: "#5f5f5f",
+    fontColor: "#fff",
+    hover: "#3b3a39",
+  },
+};
