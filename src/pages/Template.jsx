d67b4dddf384c5cd91d8bfe6343b687c5988403d
crowwan <crowwan@naver.com>
2023-04-05 19:11:21 +0900

    feat: Add open props to Nav component

--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Header from "../layouts/Header";
 import Nav from "../layouts/Nav";
+
+const SectionContainer = styled.div`
+  display: flex;
+`;
+
 function Template({ setTheme }) {
   const [open, setOpen] = useState(false);
   const onNavBtnClick = () => {
     setOpen((prev) => !prev);
   };
+
   return (
     <div>
       <Header onNavBtnClick={onNavBtnClick} setTheme={setTheme} />
-      {open && <Nav onNavBtnClick={onNavBtnClick} />}
-      <Outlet />
+      <SectionContainer>
+        <Nav onNavBtnClick={onNavBtnClick} open={open} />
+        <Outlet />
+      </SectionContainer>
     </div>
   );
 }
