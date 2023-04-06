1e0a1c87f42a2295c7fe2cd803727ea9d2dc417f
crowwan <crowwan@naver.com>
2023-04-06 15:21:02 +0900

    feat: Add border

--- src/styled/StyledInputContainer.jsx
+++ src/styled/StyledInputContainer.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledInputContainer = styled.div`
   width: 100%;
   background-color: ${(props) => props.theme.inputColor};
+  border-radius: 5px;
   padding: 1rem;
 `;
 
