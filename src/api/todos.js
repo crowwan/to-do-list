590acf11dc531317154e208e6013bd621f20701b
crowwan <crowwan@naver.com>
2023-04-07 19:13:15 +0900

    feat: proxy

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 const getTodos = () => {
-  return axios.get(`${BASE_URL}/todos`);
+  return axios.get(`/todos`);
 };
 
 const addTodo = (data) => {
-  return axios.post(`${BASE_URL}/todos`, data);
+  return axios.post(`/todos`, data);
 };
 
 const updateTodo = (id, data) => {
-  return axios.post(`${BASE_URL}/todos/${id}`, data);
+  return axios.post(`/todos/${id}`, data);
 };
 
 const deleteTodo = (id) => {
-  return axios.delete(`${BASE_URL}/todos/${id}`);
+  return axios.delete(`/todos/${id}`);
 };
 
 export { getTodos, addTodo, updateTodo, deleteTodo };
