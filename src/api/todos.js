aa42a1f977e14b1717e3bcebf99d8460ce01d3d1
crowwan <crowwan@naver.com>
2023-04-09 22:09:39 +0900

    style: 주석 제거

--- src/api/todos.js
+++ src/api/todos.js
@@ -19,27 +19,3 @@ const deleteTodo = (id) => {
 };
 
 export { getTodos, addTodo, updateTodo, deleteTodo };
-
-// [1,2,4]
-
-// const [data, setData] = useState([])
-
-// fetch('url',{
-//   method:'delete'
-// }).then(res =>
-//   res.json()).then(res => {
-//     const newData = data.filter(a => a.id !== id);
-//     setData(newData);
-// })
-// 서버에 저장된 데이터랑 클라이언트에서 사용하고 있는 상태랑 일치를 시켜야한다.
-// 서버에서 만약에 삭제된 이후에 전체데이터를 다시준다 -=>
-// 그걸 쓰거나 아니면 우리가 쓰던 상태를 가지고
-// 직접 삭제해서 쓰거나.
-
-// 함수를 쓰는데 지금 없는 외부의 코드를 써야한다. 방법이 두개다. => 인자로 받는다.
-// 서버요청에 대한 함수였다. 그러면 그냥 return fetch().then()
-
-// setTodos가 있는 곳에서 어쨌든 f().then(res => setTodos)
-
-// get
-// fetch(url).then(res => res.json()).then((res) => { setTodo(res) })
