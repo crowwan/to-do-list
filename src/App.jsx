4bde1af85b8028f479b6cb9648084c5724300643
crowwan <crowwan@naver.com>
2023-04-12 15:01:56 +0900

    comment: 테스트용 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./components/GlobalStyles";
-import routerData from "./data/routerData";
-import { setData } from "./features/dataSlice";
-import Main from "./layouts/Main";
-import LogInPage from "./pages/LogInPage";
-import Template from "./pages/Template";
-import Loading from "./ui/Loading";
-import { getTodos } from "./api/todos";
-import { themes } from "./data/themeData";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './components/GlobalStyles';
+import routerData from './data/routerData';
+import { setData } from './features/dataSlice';
+import Main from './layouts/Main';
+import LogInPage from './pages/LogInPage';
+import Template from './pages/Template';
+import Loading from './ui/Loading';
+import { getTodos } from './api/todos';
+import { themes } from './data/themeData';
 
 function App() {
   // TODO: 테마 로컬 스토리지 연동
   // TODO: 모바일 폰트 바꾸기
+  // dev test 용 주석
   const [isLoading, setIsLoading] = useState(false);
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState('dark');
   const currentUser = useSelector((s) => s.user);
   const dispatch = useDispatch();
   const navigation = useNavigate();
 
   useEffect(() => {
     if (!currentUser) {
-      navigation("/");
+      navigation('/');
     } else {
       setIsLoading(true);
       getTodos()
