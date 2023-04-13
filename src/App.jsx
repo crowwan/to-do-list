a601c3052be4a1fa4d7e985b934cc4295c1472cc
crowwan <crowwan@naver.com>
2023-04-13 22:10:36 +0900

    style: change singlequote to doublequote

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './components/GlobalStyles';
-import routerData from './data/routerData';
-import { setData } from './features/dataSlice';
-import Main from './layouts/Main';
-import LogInPage from './pages/LogInPage';
-import Template from './pages/Template';
-import Loading from './ui/Loading';
-import { getTodos } from './api/todos';
-import { themes } from './data/themeData';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/GlobalStyles";
+import routerData from "./data/routerData";
+import { setData } from "./features/dataSlice";
+import Main from "./layouts/Main";
+import LogInPage from "./pages/LogInPage";
+import Template from "./pages/Template";
+import Loading from "./ui/Loading";
+import { getTodos } from "./api/todos";
+import { themes } from "./data/themeData";
 
 function App() {
   // TODO: 테마 로컬 스토리지 연동
   // TODO: 모바일 폰트 바꾸기
   // testsdfsd
   const [isLoading, setIsLoading] = useState(false);
-  const [theme, setTheme] = useState('dark');
-  const currentUser = useSelector((s) => s.user);
+  const [theme, setTheme] = useState("dark");
+  const currentUser = useSelector(s => s.user);
   const dispatch = useDispatch();
   const navigation = useNavigate();
 
   useEffect(() => {
     if (!currentUser) {
-      navigation('/');
+      navigation("/");
     } else {
       setIsLoading(true);
       getTodos()
-        .then((res) => {
+        .then(res => {
           dispatch(setData(res.data));
           setIsLoading(false);
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err);
           setIsLoading(false);
         });
@@ -47,18 +47,9 @@ function App() {
           <Loading />
         ) : (
           <Routes>
-            <Route
-              path="/"
-              element={
-                !currentUser ? <LogInPage /> : <Template setTheme={setTheme} />
-              }
-            >
-              {routerData.map((a) => (
-                <Route
-                  key={a.name}
-                  path={a.path}
-                  element={<Main title={a.name} path={a.path} />}
-                />
+            <Route path="/" element={!currentUser ? <LogInPage /> : <Template setTheme={setTheme} />}>
+              {routerData.map(a => (
+                <Route key={a.name} path={a.path} element={<Main title={a.name} path={a.path} />} />
               ))}
             </Route>
           </Routes>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from './app/store';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./app/store";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // TODO: basename 설정할 것 (.env에 설정 안 하면 localhost로 판단한다고 함 개발 시에는 상관없지만, 빌드 시에는 바꿔야함)
 root.render(
   <Provider store={store}>
